b6df15ea1acd91f491c52b4810f8bd2b0c59efd6
Jacob M <willmoiser@gmail.com>
2025-01-15 15:00:59 -0600

    Moved them inline as needed

--- include/json_problem_parser.hpp
+++ include/json_problem_parser.hpp
@@ -7,12 +7,12 @@
 class problem_feasibility_exception : public std::runtime_error
 {
 public:
-    problem_feasibility_exception(const char *c) : std::runtime_error(c) {}
+    inline problem_feasibility_exception(const char *c) : std::runtime_error(c) {}
 
-    problem_feasibility_exception(std::string c) : std::runtime_error(c) {}
+    inline problem_feasibility_exception(std::string c) : std::runtime_error(c) {}
 
-    problem_feasibility_exception(const std::string &c) : std::runtime_error(c) {}
+    inline problem_feasibility_exception(const std::string &c) : std::runtime_error(c) {}
 };
 
-Problems parse_problems(const std::string& str);
+Problems parse_problems(const std::string &str);
 Problems parse_problems(const std::filesystem::path &json_file);
